1ff442117f85ce426f8251b6384c608e556b36c7
Al-Haq Studio <contact@alhaq.studio>
2026-08-13 21:39:45 +0100

    config: Add com.alhaq.amguard to allowed calling packages

--- app/src/main/java/com/alhaq/amnshield/api/AmnShieldApiService.kt
+++ app/src/main/java/com/alhaq/amnshield/api/AmnShieldApiService.kt
@@ -70,6 +70,7 @@ class AmnShieldApiService : Service() {
         if (packages != null && (
             packages.contains("com.alhaq.amnshield.guardian") ||
             packages.contains("com.alhaq.amnshield.guardian.debug") ||
+            packages.contains("com.alhaq.amguard") ||
             packages.contains("com.alhaq.amnshield.netblock")
         )) {
             return true
